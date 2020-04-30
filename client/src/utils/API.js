6fbd4f73e8ab64774bd7d88439ceda77857d218f
Joel Scott <jscottusf@gmail.com>
2020-04-30 14:40:03 -0400

    made a wireframe for how I'd like this to look

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default {
+  //google book by name
+  googleBooks: function (query) {
+    return axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&callback=handleResponse`
+    );
+  },
+  googleBookCategory: function (query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  },
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getBooks: function () {
+    return axios.get('/api/books');
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getBook: function (id) {
+    return axios.get('/api/books/' + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteBook: function (id) {
+    return axios.delete('/api/books/' + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+  saveBook: function (bookData) {
+    return axios.post('/api/books', bookData);
+  },
 };
