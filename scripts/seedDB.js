c2d480741ecf4f1c4f3c6e4720ddf23f7420e79f
Joel Scott <jscottusf@gmail.com>
2020-05-03 13:38:38 -0400

    adjusting mongo lab settings

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,7 +3,9 @@ const db = require('../models');
 
 // This file empties the Books collection and inserts the books below
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_react'
+);
 
 const bookSeed = [
   {
--- server.js
+++ server.js
@@ -16,7 +16,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_react'
+);
 
 // Start the API server
 app.listen(PORT, function () {
